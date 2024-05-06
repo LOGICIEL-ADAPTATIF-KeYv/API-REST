d3959b5d88c7fb956f2854732dcb75b1ba06d41f
IceK37 <45316097+IceK37@users.noreply.github.com>
2024-05-06 23:41:00 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001', // Autoriser l'accès depuis cette origine
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'], // Autoriser ces méthodes HTTP
+    allowedHeaders: ['Content-Type', 'Authorization'], // Autoriser ces en-têtes
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Logiciel adaptatif KeYv Swagger')
     .setDescription(`Toute les routes de l'api REST`)
     .setVersion('1.0')
-    .build();
+    .build(); 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
